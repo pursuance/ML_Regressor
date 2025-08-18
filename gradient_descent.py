4be9b5b6298aa7c084e433cc29e3afee69236672
Pursuance <kyletalin@gmail.com>
2025-08-18 14:38:10 -0400

    Changed data set

---
+++ gradient_descent.py
@@ -0,0 +1,52 @@
+import math
+import numpy.typing as npt
+
+def compute_cost(x: npt.NDArray , y: npt.NDArray, w, b):
+    cost = 0
+    m = x.shape[0]
+
+    for i in range(m):
+        f_wb = w*x[i] + b
+        cost = cost + (f_wb - y[i])**2
+
+    cost = cost / (2*m)
+
+    return cost
+
+def compute_gradient(x: npt.NDArray , y: npt.NDArray, w, b):
+
+    m = x.shape[0]
+    dj_dw = 0
+    dj_db = 0
+
+    for i in range(m):
+        f_wb = w*x[i] + b
+        dj_dw = dj_dw + (f_wb - y[i]) * x[i]
+        dj_db = dj_db + (f_wb - y[i])
+    
+    dj_dw = dj_dw / m
+    dj_db = dj_db / m
+
+    return dj_dw, dj_db
+
+def gradient_descent(x: npt.NDArray , y: npt.NDArray, w, b, num_iterations, alpha):
+
+    J_history = []
+    p_history = []
+
+    for i in range(num_iterations):
+        dj_dw, dj_db = compute_gradient(x, y, w, b)
+
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+
+        if i < 100001:
+            J_history.append(compute_cost(x, y, w, b))
+            p_history.append([w,b])
+
+        if i% math.ceil(num_iterations/10) == 0:
+            print(f"Iteration {i}: Cost {J_history[-1]} ",
+                  f"dj_dw: {dj_dw}, dj_db: {dj_db}  ",
+                  f"w: {w}, b:{b}")
+ 
+    return w, b, J_history, p_history
--- main.py
+++ main.py
@@ -1,19 +1,41 @@
 #%%
 import pandas as pd
+from gradient_descent import gradient_descent
+import matplotlib.pyplot as plt
+import numpy as np
 
-data = pd.read_csv('boston.csv')
-x_train = data[['RM']]
-y_train = data[['MEDV']]
+data = pd.read_csv('Car_Price_Prediction.csv')
+x_train = data[['Mileage']].to_numpy()
+y_train = data[['Price']].to_numpy()
 
-m = len(x_train)
+x_train = (x_train - x_train.mean()) / x_train.std()
+y_train = (y_train - y_train.mean()) / y_train.std()
 
-def compute_cost(x, y, w, b, m):
-    cost = 0
+w_init = 0
+b_init = 0
 
-    for i in range(m):
-        f_wb = w*x[i] + b
-        cost = cost + (f_wb + y[i])**2
+iterations = 10000
+alpha = 0.0001
 
-    cost = cost / (2*m)
+x_vals = np.linspace(x_train.min(), x_train.max(), 100)
+y_vals = w_init * x_vals + b_init
 
-    return cost
+plt.scatter(x_train, y_train, color='blue', marker='o')
+plt.plot(x_vals, y_vals, color='red', label=f'w = {w_init}, b = {b_init}')
+
+plt.xlabel('Milage')
+plt.ylabel('Price')
+
+plt.show()
+
+w_final, b_final, J_hist, p_hist = gradient_descent(x_train, y_train, w_init, b_init, iterations, alpha)
+
+y_vals = w_final * x_vals + b_final
+
+plt.scatter(x_train, y_train, color='blue', marker='o')
+plt.plot(x_vals, y_vals, color='red', label=f'w = {w_init}, b = {b_init}')
+
+plt.xlabel('Milage')
+plt.ylabel('Price')
+
+plt.show()
