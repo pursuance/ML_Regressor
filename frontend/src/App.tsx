e0abdae0e362c0097048561c4df5d761c1ff23a1
Pursuance <kyletalin@gmail.com>
2025-09-11 15:43:11 -0400

    Display final parameters

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,15 @@
 import SubmissionForm from "@/components/SubmissionForm"
+import { useFinalParametersStore } from "@/store"
 
 function App() {
+
+  const { final_w, final_b } = useFinalParametersStore()
+
   return (
     <>
       <SubmissionForm />
+      {final_w && final_w.map((w, index) => <h1 key={index}>w{index} = {w}</h1>)}
+      <h1>b = {final_b}</h1>
     </>
   )
 }
