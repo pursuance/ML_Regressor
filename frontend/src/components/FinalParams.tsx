928f104d92142040f26a574156292314d26fd750
Pursuance <kyletalin@gmail.com>
2025-10-22 13:20:42 -0400

    Added correct katex package

--- frontend/src/components/FinalParams.tsx
+++ frontend/src/components/FinalParams.tsx
@@ -1,11 +1,14 @@
 import { useFinalParametersStore, useSelectionsStore } from "@/store"
-import { InlineMath } from 'react-katex'
+import TeX from '@matejmazur/react-katex'
+import 'katex/dist/katex.min.css'
 
 
 const FinalParams = () => {
 
   const { final_w, final_b, J_history } = useFinalParametersStore()
   const { features } = useSelectionsStore()
+
+  const final_model = `y = ${final_w.map((w,index) => `${w} x${index} `)} + ${final_b}`
 
   return (
     <div className="grid grid-cols-2 gap-4 text-sm">
@@ -22,6 +25,9 @@ const FinalParams = () => {
         <h4 className="font-semibold mb-2">Final Bias:</h4>
         {final_b && <p>b: {final_b.toFixed(4)}</p>}
       </div>
+      <TeX>
+        {final_model}
+      </TeX>
     </div>
   )
 }
